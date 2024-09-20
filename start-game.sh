6ca6353217e267bdcfb2101fe180ce5fedd17ff7
guadacastro <guadycasmar123@gmail.com>
2024-09-20 16:07:08 +0200

    write outskirts logic

--- start-game.sh
+++ start-game.sh
@@ -5,21 +5,45 @@ root_path=$(pwd)
 key=false
 shabby_sword=false
 legendary_sword=false
-armour=false
+armor=false
 coins=0
 amulet=false
 
 function game_loop {
     current_path=$(pwd)
-    echo $root_path
-    echo $current_path
-    # cat "${current_path}/story"
+    # echo $root_path
+    # echo $current_path
     
-    if ["$amulet" == false] || [[$PWD == "{$root_path}/Town_square/outskirts/cave/left"]]; then
-        echo "The door is locked.
-        You do not have the key for it and you aren't strong enough to break it.
-        Try coming back later."
-    fi   
+    if [[ "$amulet" == false ]] && [[ "$key" == false ]] && [[ $PWD == "$root_path/Town_square/outskirts/cave/left" ]]; then
+        echo "The door is locked.\n You do not have the key for it and you aren't strong enough to break it.\n Try coming back later."
+        cd ..
+    elif [[ "$amulet" == true ]] && [[ $PWD == "$root_path/Town_square/outskirts/cave/left" ]]; then
+        echo "You broke the door, congratulations!"
+    elif [[ "$key" == true ]] && [[ $PWD == "$root_path/Town_square/outskirts/cave/left" ]]; then
+        echo "You have unlocked the door, congratulations!"
+    fi
+
+    if [[ $PWD == "$root_path/Town_square/outskirts/cave/left/armory" ]]; then
+        armor=true
+    fi
+
+    if [[ $PWD == "$root_path/Town_square/outskirts/cave/front" ]]; then
+        coins=$((coins+50))
+    fi
+
+    if [[ $PWD == "$root_path/Town_square/outskirts/cave/right" ]]; then
+        coins=$((coins+50))
+    fi
+
+    if [[ $PWD == "$root_path/Town_square/outskirts/market/blacksmith" ]] && [[ "$coins" < 100 ]]; then
+        echo "You see a blacksmith selling swords but you don't have anough money to buy one.\n Come back once you earn more (try searching the cave)"
+        cd ..
+    elif [[ $PWD == "$root_path/Town_square/outskirts/market/blacksmith" ]] && [[ "$coins" >= 100 ]]; then
+        shabby_sword=true
+        coins=0
+    fi
+
+    cat "${current_path}/story"
 }
 
 function custom_cd {
